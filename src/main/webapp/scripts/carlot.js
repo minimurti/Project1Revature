6026d030a8c9b8693c0d6e4328c2e7dac50fd913
minimurti <33699412+minimurti@users.noreply.github.com>
2020-03-11 09:35:03 -0400

    Almost there...

--- src/main/webapp/scripts/carlot.js
+++ src/main/webapp/scripts/carlot.js
@@ -2,33 +2,56 @@ function getCarList() {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            let carList = JSON.parse(xhr.responseText);
-            console.log(carList);
-            displayCarList(carList);
+            let formList = JSON.parse(xhr.responseText);
+            console.log(formList);
+            displayCarList(formList);
         }
     }
     xhr.open("GET", "/CarDealership/car", true);
     xhr.send();
 }
 
-function displayCarList(carList){
+function displayCarList(formList){
     
-    for(let car of carList) {
+    for(let form of formList) {
         let row = document.createElement("tr");
-        row.className = "list-form"
-        let make = document.createElement("td");
-        let model = document.createElement("td");
-        let year = document.createElement("td");
-        let vin = document.createElement("td");
-        make.innerHTML = car.make;
-        model.innerHTML = car.model;
-        year.innerHTML = car.year;
-        vin.innerHTML = car.vin;
-        row.appendChild(make);
-        row.appendChild(model);
-        row.appendChild(year);
-        row.appendChild(vin);
+        row.className = "list-form";
+        let description = document.createElement("td");
+        let location = document.createElement("td");
+        let submit_date = document.createElement("td");
+        let start_date = document.createElement("td");
+        let cost = document.createElement("td");
+        let grading_format = document.createElement("td");
+        let ds_approved = document.createElement("td");
+        let dh_approved = document.createElement("td");
+        let bc_approved = document.createElement("td");
+        
+        description.innerHTML = form.description;
+        location.innerHTML = form.location;
+        submit_date.innerHTML = form.submit_date;
+        start_date.innerHTML = form.start_date;
+        cost.innerHTML = form.cost;
+        grading_format.innerHTML = form.grading_format;
+        ds_approved.innerHTML = form.ds_approved;
+        dh_approved.innerHTML = form.dh_approved;
+        bc_approved.innerHTML = form.bc_approved;
+        
+        
+        row.appendChild(description);
+        row.appendChild(location);
+        row.appendChild(submit_date);
+        row.appendChild(start_date);
+        row.appendChild(cost);
+        row.appendChild(grading_format);
+        row.appendChild(ds_approved);
+        row.appendChild(dh_approved);
+        row.appendChild(bc_approved);
         document.getElementById("carTable").appendChild(row);
+
+
+        
+       
+                
     }
 }
 
